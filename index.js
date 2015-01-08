320d4ca8c84a5bd20524d3b244cb823ffb5c55d8
Gurdas Nijor <nijor22@gmail.com>
2015-01-08 00:28:47 -0800

    Cleanup loader index

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ var escope = require('escope');
 
 
 var transformModule = require('./transformers/module');
-var entryModule = require('./transformers/entry_module');
-
-
-//Regex to figure out if anything "Reacty" is in the module
-//TODO:  Use less stupid way of determining this
-var REACT_CLASS_RE = /\React.createClass/;
-
+var transformEntryModule = require('./transformers/entry_module');
 
 
 
@@ -32,61 +26,41 @@ function getEntryArray(entry){
 
 module.exports = function(source, map) {
 
-  var self = this;
   var query = loaderUtils.parseQuery(this.query);
   var importLoaders = parseInt(query.importLoaders, 10) || 0;
+  var resourcePath = this.resourcePath;
+  var filename = path.basename(resourcePath);
+  var componentPath = this.context; //Make this a user provided parameter to determine the component dir
 
-
- // console.log( this.loaders.map(function(e){return e.request}));
+  var ignoredFiles = ['autocat_index.js'];
 
   if (this.cacheable) {
     this.cacheable();
   }
 
-  var resourcePath = this.resourcePath,
-    filename = path.basename(resourcePath);
-
+  if (/node_modules/.test(this.context) || _.contains(ignoredFiles, filename)) {
+    return source;
+  }
 
   //Parse out all potential app entry points
   var entryPaths = !_.isPlainObject(this.options.entry) ?
     getEntryArray(this.options.entry) :
     _(this.options.entry).map(function(val, key){ return getEntryArray(val); }).flatten().value();
 
+  var isEntryModule = _.any(entryPaths, function(e){ return resourcePath.indexOf(e.replace(".", "")) !== -1 });
 
 
-  var ignoredFiles = ['autocat_index.js', 'Glyphicon.jsx', 'OverlayTrigger.jsx'];
-
-
-  if (!/node_modules/.test(this.context) && !_.contains(ignoredFiles, filename)){
-
-
-    var curPath = this.resourcePath;
-
-    //The entry module(s) we're going to hijack with AutoCat
-    if( _.any(entryPaths, function(e){ return curPath.indexOf(e.replace(".", "")) !== -1 })){
-
-      //This is the dir that gets traversed when building out the catalog
-      //TODO:  Allow a path relative to the webpack config file to be passed in a loader config parameter
-      //To override this behavior of traversing the directory that the entry module is located in
-      var componentPath = this.context;
-
-      return entryModule(source, componentPath);
-    }
-
-    else{
-
-      //Ignore modules without React top level api calls
-      if (!source.match(REACT_CLASS_RE)) {
-        return source;
-      }
-
-      return transformModule(source, filename, resourcePath);
-    }
+  if(isEntryModule){
+    return transformEntryModule(source, componentPath);
   }
-
+  //Only transform source files that define components
+  else if (source.match(/\React.createClass/)){
+    return transformModule(source, filename, resourcePath)
+  }
   else{
     return source;
   }
+
 }
 
 
